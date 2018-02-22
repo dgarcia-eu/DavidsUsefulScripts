8f4142114054b33bad0dcc654eb73fc7233a38aa
David Garcia <dgarcia@ethz.ch>
2018-02-22 14:26:34 +0100

    Add files via upload

---
+++ 01_getFollowerCounts.py
@@ -0,0 +1,36 @@
+import sys
+import time
+import json
+import tweepy
+import datetime
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+auth = tweepy.OAuthHandler(str(sys.argv[2]),str(sys.argv[3]))
+#"BkAI8TjroDvwkHZwrVLLivPIW","gbJDpJyuALCWgrAofzCZM48MQzuYMmJk4IH12GyqOEexHZFmqq"
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, compression=True)
+
+fin = open(sys.argv[1], "rt")
+querylist = list()
+allusersdata = list()
+
+for line in fin:
+	querylist.append(line.replace("\n",""))
+	if len(querylist) == 100:
+		try:
+			usersdata = api.lookup_users(screen_names=querylist)				
+		except tweepy.TweepError as e:
+			print e
+		allusersdata.extend(usersdata)
+		querylist = list()
+
+if len(querylist) > 0:
+	try:
+		usersdata = api.lookup_users(screen_names=querylist)				
+	except tweepy.TweepError as e:
+		print e
+	allusersdata.extend(usersdata)
+
+for ud in allusersdata:
+	print ud.screen_name + "\t" + str(ud.followers_count)
