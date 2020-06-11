7e7a01f6cf63d687d0d49c07bbecc2d68a5a0ddd
dgarcia-eu <garcia@csh.ac.at>
2020-06-11 22:11:10 +0200

    200 tweets timeline version

---
+++ getUserTimeline200.py
@@ -0,0 +1,29 @@
+import sys
+import time
+import json
+import tweepy
+import datetime
+
+auth = tweepy.OAuthHandler(str(sys.argv[3]),str(sys.argv[4]))
+api = tweepy.API(auth,wait_on_rate_limit=True)
+
+folder = str(sys.argv[2])
+fin = open(sys.argv[1], "rt")
+
+linebuffer = list()
+for line in fin:
+	uid = int(line.replace("\n",""))
+	time.sleep(1) # some waiting time to try to keep the 100K daily limit of Twitter
+	try:
+		with open(folder+"/"+str(uid)+".json", "wt") as fout:
+			for tweet in tweepy.Cursor(api.user_timeline, id=uid, include_rts=True, exclude_replies=False, tweet_mode='extended').items(200):
+				fout.write(json.dumps(tweet._json) + "\n")
+	except tweepy.TweepError as e:
+		print(e)
+fin.close()
+
+
+
+
+
+
